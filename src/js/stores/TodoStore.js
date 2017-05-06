b214991922579978b840f1821c606d7ee996afde
Mary Gao <maryqygao@gmail.com>
2017-05-06 06:32:47 -0700

    Add flux dispatcher for Todos

--- src/js/stores/TodoStore.js
+++ src/js/stores/TodoStore.js
@@ -1,4 +1,5 @@
 import { EventEmitter } from 'events';
+import dispatcher from '../dispatcher';
 
 class TodoStore extends EventEmitter {
   todos = [
@@ -7,6 +8,20 @@ class TodoStore extends EventEmitter {
     { id: 57838337, text: 'Make Dinner', complete: false },
     { id: 34617435, text: 'Learn React', complete: false }
   ];
+
+  actionHandlers = {
+    CREATE_TODO: action => {
+      this.createTodo(action.text);
+    }
+  };
+
+  handleActions = action => {
+    const handlerFunc = this.actionHandlers[action.type];
+
+    if (typeof handlerFunc === 'function') {
+      handlerFunc(action);
+    }
+  };
 
   createTodo(text) {
     const id = Date.now();
@@ -22,4 +37,6 @@ class TodoStore extends EventEmitter {
 }
 
 const todoStore = new TodoStore();
+dispatcher.register(todoStore.handleActions);
+
 export default todoStore;
